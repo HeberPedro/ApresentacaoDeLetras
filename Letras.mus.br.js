374e1a0282e385b7bae6ccc3c0e3e5da59aa9d84
Heber V <heberpedro@outlook.com>
2019-10-31 07:41:22 -0300

    Update .js

--- Letras.mus.br.js
+++ Letras.mus.br.js
@@ -27,10 +27,9 @@ function loadPage(urlCurrent) {
             document.write("<head>\n");
                 document.write("<meta charset='utf-8'>\n"+
                 "<meta name='viewport' content='width=device-width, initial-scale=1'>\n"+
-                "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\n"+
-                "<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\n"+
-                "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\n" +
-                "<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\n" +
+                "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>\n"+
+                "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>\n"+
+                "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>\n" +
                 "<style>\n" +
                     "body{background-color:rgb(35,31,32);font-family:Cambria,Georgia,serif;word-wrap:break-word;text-align:center;margin:0;overflow-x:hidden;overflow-y:hidden}\n" +
                     "h1{color:rgb(255,102,0);font-size:75px;font-weight:700;padding-top:40px}\n" +
